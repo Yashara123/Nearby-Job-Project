b1731779c5057fc224bc1ce42b74c787ae03dca1
Yashara123 <saarasharindi@gmail.com>
2026-05-12 18:12:52 +0530

    Setup project structure, connect MongoDB and add Job model

---
+++ models/models.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const JobSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    location: { type: String, required: true },
+    salary: { type: Number },
+    category: { type: String },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Job', JobSchema);
