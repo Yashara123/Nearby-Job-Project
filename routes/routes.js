eb0d71399c9db2dcd70463e2886c454d1529d0c5
Yashara123 <saarasharindi@gmail.com>
2026-05-17 23:30:49 +0530

    create presentation

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 const jobController = require('../controllers/jobController');
 
+
 router.post('/', jobController.createJob);
+
+
 router.get('/', jobController.getAllJobs);
+
+
+router.put('/:id', jobController.updateJob);
+
+
+router.delete('/:id', jobController.deleteJob);
 
 module.exports = router;
